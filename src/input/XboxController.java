a4d877eb46a94f4f11561933ab53d6e2398e02d6
SecondThread <13dharmeyer@gmail.com>
2016-07-22 15:06:59 -0500

    added comments for xbox controller

--- src/input/XboxController.java
+++ src/input/XboxController.java
@@ -1,10 +1,41 @@
 package input;
 
+import robot.IControl;
+
 /**
- * A class that will read from Xbox Controller inputs and make the Xbox controller rumble.
- * I will do this eventually
- * --SecondThread
+ * A class that will read from Xbox Controller inputs and make the Xbox
+ * controller rumble. I will do this eventually --SecondThread
  */
-public class XboxController {
+public class XboxController extends IControl {
+	/**
+	 * The singleton instance of this class. <i>xboxController</i> is null if
+	 * the singleton has not been instantiated yet.
+	 */
+	private static XboxController xboxController;
+
+	/**
+	 * Gets the singleton xboxController instance. Only one xboxController is
+	 * ever created so that different systems get consistent readings.
+	 * <br>
+	 * <br>
+	 * Preconditions: none<br>
+	 * Postconditions: none<br>
+	 * 
+	 * @return The singleton of this class
+	 */
+	public static XboxController getXboxController() {
+		if (xboxController==null) {
+			xboxController=new XboxController();
+		}
+		return xboxController;
+	}
+
+	/**
+	 * The private constructor that sets up the xboxController
+	 * (preconditions and postconditions are not required for private methods, doesn't return anything.
+	 */
+	private XboxController() {
+
+	}
 
 }
