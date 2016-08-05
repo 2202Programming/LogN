40fc1ee7f5eb562a9c21b88ba9d9774a0e6efe6c
leinad2018 <leinad2018@gmail.com>
2016-08-04 23:40:05 -0500

    Damage Control
    Note: pay attention when solving merge conflicts

--- src/drive/ArcadeDrive.java
+++ src/drive/ArcadeDrive.java
@@ -1,123 +1,8 @@
 package drive;
 
+import comms.XboxController;
 import physicalOutput.IMotor;
 
-/**
- *
- */
-public class ArcadeDrive extends IDrive {
-
-	/**
-	 * True if the wheel motors are being controlled by ArcadeDrive, false
-	 * otherwise.
-	 */
-	private boolean enabled;
-
-	/**
-	 * The motor corresponding to the front right wheel
-	 */
-	private IMotor frontRight;
-
-	/**
-	 * The motor corresponding to the front left wheel
-	 */
-	private IMotor frontLeft;
-
-	/**
-	 * The motor corresponding to the back right wheel
-	 */
-	private IMotor backRight;
-
-	/**
-	 * The motor corresponding to the back left wheel
-	 */
-	private IMotor backLeft;
-
-	/**
-	 * @param fl
-	 *            The front left motor
-	 * @param fr
-	 *            The front right motor
-	 * @param bl
-	 *            The back left motor
-	 * @param br
-	 *            The back right motor
-	 */
-	public ArcadeDrive(IMotor fl, IMotor fr, IMotor bl, IMotor br) {
-		enabled =true;
-		this.frontLeft=fl; //TODO this.* statements are redundent.
-		this.frontRight=fr;
-		this.backLeft=bl;
-		this.backRight=br;
-	}
-
-	/**
-	 *
-	 */
-	public void teleopInit() {
-		// TODO implement here
-	}
-
-	/**
-	 *
-	 */
-	public void teleopPeriodic() {
-		// TODO implement here
-	}
-
-	/**
-	 * Sets the left side motor power<br>
-	 * Preconditions: speed is between -1.0 and 1.0<br>
-	 * Postconditions: sets the motors
-	 */
-	public void setLeftMotors(double speed) {
-		if (enabled) {
-			frontLeft.setSpeed(speed);
-			backLeft.setSpeed(speed);
-		} // TODO If they are not enabled you should set them to zero.
-	}
-
-	/**
-	 * Sets the right side motor power<br>
-	 * Preconditions: speed is between -1.0 and 1.0<br>
-	 * Postconditions: sets the motors
-	 */
-	public void setRightMotors(double speed) {
-		if (enabled) {
-			frontRight.setSpeed(speed);
-			backRight.setSpeed(speed);
-		}
-	}
-
-	/**
-	 * Sets whether or not arcade drive should control the motors (disable if
-	 * something else is taking control) <br>
-	 * <br>
-	 * Preconditions: none <br>
-	 * Postconditions: If ArcadeDrive has been set to enabled, it will continue
-	 * (or begin) to write to the motors. Otherwise, the motors will not be set.
-	 *
-	 * @param enabled
-	 *            Whether or not this <i>ArcadeDrive</i> object should be
-	 *            enabled.
-	 */
-	public void setEnabled(boolean enabled) {
-		this.enabled=enabled;
-	}
-
-	/**
-	 * Checks to see if this <i>ArcadeDrive</i> is enabled or not
-	 *
-	 * @return True if enabled, false otherwise
-	 */
-	public boolean getEnabled() {
-		return enabled;
-	}
-
-}
-
-import input.XboxController;
-import motors.IMotor;
 
 /**
  * An arcade drive that uses only the left JoyStick to move. <br>
@@ -242,11 +127,8 @@ public class ArcadeDrive extends IDrive {
 	 * them do
 	 */
 	public boolean hasEncoders() {
-		boolean toReturn=false;
-		if (frontLeft.hasEncoder()||frontRight.hasEncoder()||backLeft.hasEncoder()||backRight.hasEncoder()) {
-			toReturn=true;
-		}
-		return toReturn;
+		//TODO implement in SensorController branch
+		return false;
 	}
 
 	// comments in IDrive, implementation is obvious
--- src/drive/IDrive.java
+++ src/drive/IDrive.java
@@ -4,29 +4,6 @@ import robot.IControl;
 
 /**
  * This class is for any types of drive we choose to implement in the future
- * @author Daniel
- *
- */
-public abstract class IDrive extends IControl {
-	//All drive classes must override all of the methods below
-	public abstract void teleopInit();
-	
-	public abstract void teleopPeriodic();
-	
-	public abstract void setLeftMotors(double speed);
-	
-	public abstract void setRightMotors(double speed);
-	
-	public abstract void setEnabled(boolean enabled);
-	
-	public abstract boolean getEnabled();
-
-}
-
-import robot.IControl;
-
-/**
- * This class is for any types of drive we choose to implement in the future
  * 
  * @author SecondThread
  *
