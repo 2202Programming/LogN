320e832cc8a854d78225360ab3db4ec65183d275
SecondThread <13dharmeyer@gmail.com>
2016-08-21 20:09:36 -0500

    simplified, and changed some var names

--- src/other/PIDController.java
+++ src/other/PIDController.java
@@ -6,20 +6,21 @@ public class PIDController {
 	private double kd;
 	private double totalError;
 	private double lastError;
-	private boolean advanced;
+	private boolean resetOnOvershoot;
 
 	/**
 	 * A default constructor for a PIDController<br>
 	 * Must set pid values before using calculate
 	 */
-	public PIDController(){
-		kp = 0;
-		ki = 0;
-		kd = 0;
-		totalError = 0;
-		lastError = 0;
-		advanced = false;
+	public PIDController() {
+		kp=0;
+		ki=0;
+		kd=0;
+		totalError=0;
+		lastError=0;
+		resetOnOvershoot=false;
 	}
+
 	/**
 	 * A basic PIDController for random stuff(Can be improved greatly)
 	 * 
@@ -29,29 +30,30 @@ public class PIDController {
 	 *            Integral Constant
 	 * @param kd
 	 *            Derivative Constant
-	 * @param advanced
-	 *            If the controller should use the extra features(If you don't
-	 *            know, don't use it)
+	 * @param resetOnOvershoot
+	 *            Sets whether the I value should be reset when it is overshot
+	 *            (Use if you aren't using d)
 	 */
-	public PIDController(double kp, double ki, double kd, boolean advanced) {
-		this.kp = kp;
-		this.ki = ki;
-		this.kd = kd;
-		totalError = 0;
-		lastError = 0;
-		this.advanced = advanced;
+	public PIDController(double kp, double ki, double kd, boolean resetOnOvershoot) {
+		this.kp=kp;
+		this.ki=ki;
+		this.kd=kd;
+		totalError=0;
+		lastError=0;
+		this.resetOnOvershoot=resetOnOvershoot;
 	}
-	
+
 	/**
 	 * Sets new values for the pid variables
+	 * 
 	 * @param kpIn
 	 * @param kiIn
 	 * @param kdIn
 	 */
-	public void setPID(double kpIn, double kiIn, double kdIn){
-		kp = kpIn;
-		ki = kiIn;
-		kd = kdIn;
+	public void setPID(double kpIn, double kiIn, double kdIn) {
+		kp=kpIn;
+		ki=kiIn;
+		kd=kdIn;
 	}
 
 	/**
@@ -66,17 +68,17 @@ public class PIDController {
 	 */
 	public double calculate(double setValue, double input) {
 
-		double error = input - setValue;
+		double error=input-setValue;
 
-		if (advanced) if ((error > 0 && lastError < 0) || (error < 0 && lastError > 0)) totalError = 0;
-		totalError += error;
+		if (resetOnOvershoot) if (Math.signum(error)!=Math.signum(lastError)) totalError=0;
+		totalError+=error;
 
-		double pChange = kp * error;
-		double iChange = ki * totalError;
-		double dChange = kd * (error - lastError);
+		double pChange=kp*error;
+		double iChange=ki*totalError;
+		double dChange=kd*(error-lastError);//this really seams wrong, idk though
 
-		lastError = error;
+		lastError=error;
 
-		return pChange + iChange + dChange;
+		return pChange+iChange+dChange;
 	}
 }
