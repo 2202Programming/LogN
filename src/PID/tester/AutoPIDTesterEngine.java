513942f0079bfd83f7b174a09faf4daccd53c051
SecondThread <13dharmeyer@gmail.com>
2016-08-21 21:26:36 -0500

    Need to commit before I branch

---
+++ src/PID/tester/AutoPIDTesterEngine.java
@@ -0,0 +1,21 @@
+package PID.tester;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class AutoPIDTesterEngine {
+	
+	public AutoPIDTesterEngine() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, AutoPIDTesterWindow.WIDTH, AutoPIDTesterWindow.HEIGHT);
+	}
+	
+}
--- src/PID/tester/AutoPIDTesterWindow.java
+++ src/PID/tester/AutoPIDTesterWindow.java
@@ -10,27 +10,48 @@ import javax.swing.JPanel;
 public class AutoPIDTesterWindow {
 	private JFrame frame;
 	private JPanel mainPanel;
-	private static final int WIDTH=600, HEIGHT=400;
+	public static final int WIDTH=600, HEIGHT=400;
 
 	public static void main(String[] args) {
 		AutoPIDTesterWindow window=new AutoPIDTesterWindow();
+		window.runLoop();
+	}
+	
+	public void runLoop() {
+		long lastUpdateTime=System.currentTimeMillis();
+		long lastSecondTime=lastUpdateTime;
+		final long updatesPerSecond=60;
+		final long timeBetweenUpdates=1000/updatesPerSecond, timeBetweenSeconds=1000;
+		int renders=0, updates=0;
+		while (true) {
+			
+			if (System.currentTimeMillis()>lastSecondTime+timeBetweenSeconds) {
+				System.out.println("Updates: "+updates+"    FPS: "+renders);
+				renders=0;
+				updates=0;
+				lastSecondTime+=timeBetweenSeconds;
+			}
+			renders++;
+			draw();
+		}
 	}
 	
 	public AutoPIDTesterWindow() {
 		frame=new JFrame("Auto PID tester");
 		mainPanel=new JPanel();
-		mainPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		frame.add(mainPanel);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 	
 	public void draw() {
 		Graphics2D g=(Graphics2D)frame.getGraphics();
 		g.setColor(Color.black);
-		g.drawRect(0, 0, WIDTH, HEIGHT);
+		g.fillRect(0, 0, WIDTH-5, HEIGHT-5);
 		g.dispose();
 	}
 
