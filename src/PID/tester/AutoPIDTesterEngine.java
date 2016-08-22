322196d31d4a1ac6d96d7dc59c9f156b51d49f7b
SecondThread <13dharmeyer@gmail.com>
2016-08-22 17:43:55 -0500

    Everything probably actually fixed for real

--- src/PID/tester/AutoPIDTesterEngine.java
+++ src/PID/tester/AutoPIDTesterEngine.java
@@ -21,11 +21,15 @@ public class AutoPIDTesterEngine {
 	public void render(Graphics2D g) {
 		g.setColor(Color.darkGray);
 		g.fillRect(0, 0, AutoPIDTesterWindow.WIDTH, AutoPIDTesterWindow.HEIGHT);
+		g.setColor(Color.lightGray);
+		g.fillRect(AutoPIDTesterWindow.WIDTH/2-1, AutoPIDTesterWindow.HEIGHT/6, 1, AutoPIDTesterWindow.HEIGHT/6*2);
 		AffineTransform at=new AffineTransform();
 		at.rotate(angle, AutoPIDTesterWindow.WIDTH/2, AutoPIDTesterWindow.HEIGHT/2);
 		g.setTransform(at);
 		g.setColor(Color.red);
 		g.fillRect(AutoPIDTesterWindow.WIDTH/3, AutoPIDTesterWindow.HEIGHT/3, AutoPIDTesterWindow.WIDTH/3, AutoPIDTesterWindow.HEIGHT/3);
+		g.setColor(new Color(150, 0, 0));
+		g.fillRect(AutoPIDTesterWindow.WIDTH/2-2, AutoPIDTesterWindow.HEIGHT/3, 4, AutoPIDTesterWindow.HEIGHT/12);
 		at.rotate(-angle,AutoPIDTesterWindow.WIDTH/2, AutoPIDTesterWindow.HEIGHT/2);
 	}
 	
