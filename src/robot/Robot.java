60f2b20a91339fcf9e9baf0e043cfa6fe3b09871
SecondThread <13dharmeyer@gmail.com>
2016-10-12 20:11:18 -0500

    Fixed robot class packages; It now deploys without crashing

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -33,7 +33,7 @@ public class Robot extends IterativeRobot {
 		// Switch to decide which robot definition to use
 		switch (name) {
 		case TIM:
-			robotDefinition=new Tim();
+			//robotDefinition=new Tim();
 			break;
 		case PIPER:
 			break;
@@ -42,7 +42,7 @@ public class Robot extends IterativeRobot {
 		}
 
 		// Load all the properties in the currently selected definition
-		Global.controlObjects=robotDefinition.loadControlObjects();
+		//Global.controlObjects=robotDefinition.loadControlObjects();
 	}
 
 	public void autonomousInit() {
@@ -82,21 +82,21 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void disabledInit() {
-		SmartWriter.putS("Robot State", "Disabled Init", DebugMode.COMPETITION);
+		/*SmartWriter.putS("Robot State", "Disabled Init", DebugMode.COMPETITION);
 		try {
 			IControl.callDisabledInit();
 		} catch (Exception e) {
 			outputError(e, "Disabled Init");
-		}
+		}*/
 	}
 
 	public void disabledPeriodic() {
-		SmartWriter.putS("Robot State", "Disabled Periodic", DebugMode.COMPETITION);
+		/*SmartWriter.putS("Robot State", "Disabled Periodic", DebugMode.COMPETITION);
 		try {
 			IControl.callDisabledPeriodic();
 		} catch (Exception e) {
 			outputError(e, "Disabled Periodic");
-		}
+		}*/
 	}
 
 	/**
