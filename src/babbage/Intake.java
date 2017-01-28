2977f03dd031b5d5e9e4f0d5397da80f65e3baf5
jaime <jmmman@debian>
2017-01-28 11:42:05 -0600

    Updated the way we togle speeds for the intake

--- src/babbage/Intake.java
+++ src/babbage/Intake.java
@@ -8,43 +8,50 @@ import robot.IControl;
 public class Intake extends IControl {
 	private IMotor[] intakeMotors;
 	private XboxController controller;
-	private double speed=0;
-	
-	//775 motors
-	//2 motors ports 4 and 5
+	private double speed = 0;
+	private boolean isZero = true;
+
+	// 775 motors
+	// 2 motors ports 4 and 5
 	public Intake() {
-		intakeMotors=new SparkMotor[2];
-		intakeMotors[0]=new SparkMotor(4, true);
-		intakeMotors[1]=new SparkMotor(5, true);
+		intakeMotors = new SparkMotor[2];
+		intakeMotors[0] = new SparkMotor(4, true);
+		intakeMotors[1] = new SparkMotor(5, true);
 	}
-	
+
 	public void robotInit() {
 	}
 
 	public void autonomousInit() {
-		controller=XboxController.getXboxController();
+		controller = XboxController.getXboxController();
 	}
 
 	public void autonomousPeriodic() {
 		update();
 	}
-	
+
 	public void teleopInit() {
-		controller=XboxController.getXboxController();
+		controller = XboxController.getXboxController();
 	}
 
 	public void teleopPeriodic() {
 		update();
 	}
-	
+
 	private void update() {
-		if (controller.getAHeld()) {
-			speed=1;
+		if (controller.getAPressed()) {
+			if (isZero) {
+				speed = .5;
+				isZero = false;
+			} else if (speed == .8) {
+				speed = 0;
+				isZero = true;
+			} else {
+				speed += .1;
+			}
 		}
-		else {
-			speed=.5;
-		}
-		for (IMotor motor:intakeMotors) {
+
+		for (IMotor motor : intakeMotors) {
 			motor.setSpeed(speed);
 		}
 	}
