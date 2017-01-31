0c6b8714a343293ec04ca9e775cd5df8d29a0b70
SecondThread <13dharmeyer@gmail.com>
2017-01-30 19:31:03 -0600

    finished intake for final version

--- src/babbage/Intake.java
+++ src/babbage/Intake.java
@@ -1,69 +1,55 @@
-package babbage; 
+package babbage;
 
- 
- import comms.XboxController; 
- import physicalOutput.IMotor; 
- import physicalOutput.SparkMotor; 
- import robot.IControl; 
- 
- 
- public class Intake extends IControl { 
- 	private IMotor[] intakeMotors; 
- 	private XboxController controller; 
- 	private double speed = 0; 
- 	private boolean isZero = true; 
- 
- 
- 	// 775 motors 
- 	// 2 motors ports 4 and 5 
- 	public Intake() { 
- 		intakeMotors = new SparkMotor[2]; 
- 		intakeMotors[0] = new SparkMotor(4, true); 
- 		intakeMotors[1] = new SparkMotor(5, true); 
- 	} 
- 
- 
- 	public void robotInit() { 
- 	} 
- 
- 
- 	public void autonomousInit() { 
- 		controller = XboxController.getXboxController(); 
- 	} 
- 
- 
- 	public void autonomousPeriodic() { 
- 		update(); 
- 	} 
- 
- 
- 	public void teleopInit() { 
- 		controller = XboxController.getXboxController(); 
- 	} 
- 	
- 
- 	public void teleopPeriodic() { 
- 		update(); 
- 	} 
- 
- 
- 	private void update() { 
- 		if (controller.getAPressed())
- 		{
- 			if (speed == 0)
- 				speed = .5;
- 			else 
- 				speed += .1;
- 		}
- 		else if (controller.getBPressed())
- 		{
- 			if (speed == .5)
- 				speed = 0;
- 			else 
- 				speed -= .1;
- 		}
- 		for (IMotor motor : intakeMotors) { 
- 			motor.setSpeed(speed); 
- 		} 
- 	} 
- } 
+import comms.XboxController;
+import physicalOutput.IMotor;
+import physicalOutput.SparkMotor;
+import robot.IControl;
+
+public class Intake extends IControl {
+	private IMotor[] intakeMotors;
+	private XboxController controller;
+	private double speed=0;
+
+	// 775 motors
+	// 2 motors ports 4 and 5
+	public Intake() {
+		intakeMotors=new SparkMotor[2];
+		intakeMotors[0]=new SparkMotor(4, true);
+		intakeMotors[1]=new SparkMotor(5, true);
+	}
+
+	public void robotInit() {
+	}
+
+	public void autonomousInit() {
+		controller=XboxController.getXboxController();
+	}
+
+	public void autonomousPeriodic() {
+		update();
+	}
+
+	public void teleopInit() {
+		controller=XboxController.getXboxController();
+	}
+
+	public void teleopPeriodic() {
+		update();
+	}
+
+	public void disabledInit() {
+		speed=0;
+	}
+
+	private void update() {
+		if (controller.getAHeld()) {
+			speed=0.8;
+		}
+		else {
+			speed=0.5;
+		}
+		for (IMotor motor : intakeMotors) {
+			motor.setSpeed(speed);
+		}
+	}
+}
