5fab5a26c97f5dca7d24544a6540e9d18600df18
Archie <Archiez0077@gmail.com>
2017-02-06 21:08:32 -0600

    Started xbox update

---
+++ src/robotDefinitions/ProfileControls.java
@@ -0,0 +1,72 @@
+package robotDefinitions;
+
+import comms.XboxController;
+
+/**
+ * TODO: Add the implementation for Babbage control methods
+ *       Add methods for Piper
+ */
+public class ProfileControls {
+
+	private XboxController controller;
+
+	public ProfileControls() {
+		controller = XboxController.getXboxController(1);
+	}
+
+	// gearHolder - places gear on peg
+	public void gearHolder() {
+
+	}
+
+	// speeds up intake
+	public void intakeSpeed() {
+
+	}
+
+	// starts shooting
+	public void startShooting() {
+
+	}
+
+	// stops shooting
+	public void stopShooting() {
+
+	}
+
+	// reverse shooters, unclogs balls
+	public void reverseShooter() {
+
+	}
+
+	// turns left and right
+	public void joystickX() {
+
+	}
+
+	// foward and reverse
+	public void joystickY() {
+
+	}
+
+	// engage high-goal vision
+	public void hgVision() {
+
+	}
+
+	// engage gear peg vision
+	public void gpVision() {
+
+	}
+
+	// cancel high-goal vision
+	public void cancelHg() {
+
+	}
+
+	// cancel high-goal vision
+	public void cancelGp() {
+
+	}
+
+}
