f9c86fc59b3082d2fe0eae45b24643f50476621b
Archie <Archiez0077@gmail.com>
2017-02-11 10:09:57 -0600

    Added methods to ControlBase
    Added rightJoystick methods to ControlBase. Also added parameters to them for the controller port, and changed the XboxController in ArcadeDrive to a ControlBase.

--- src/robotDefinitions/ControlBase.java
+++ src/robotDefinitions/ControlBase.java
@@ -13,14 +13,59 @@ public abstract class ControlBase extends IControl {
 
 	public abstract XboxController[] getControllers();
 
+	/**
+	 * getLeftJoystickX - returns the x value of the controller at controllers[port]
+	 * @param port
+	 * @return
+	 */
+	public double getLeftJoystickX(int port) {
+		return controllers[port].getLeftJoystickX();
+	}
+
+	/**
+	 * getLeftJoystickY - returns the y value of the controller at controllers[port]
+	 * @param port
+	 * @return
+	 */
+	public double getLeftJoystickY(int port) {
+		return controllers[port].getLeftJoystickY();
+	}
+	/** getRightJoystickX - returns the x value of the right joystick of controller
+	 *  
+	 * @return
+	 */
+	public double getRightJoystickX(int port) {
+		return controllers[port].getRightJoystickX();
+	}
+	/**getLeftJoystickY - returns the y value of the right joystick of controller
+	 * 
+	 * @return
+	 */
+	public double getRightJoystickY(int port) {
+		return controllers[port].getRightJoystickY();
+	}
 	// turns left and right
-	public double getLeftJoystickX() {
-		return controllers[0].getLeftJoystickX();
-	}
+		public double getLeftJoystickX() {
+			return controllers[0].getLeftJoystickX();
+		}
 
-	// foward and reverse
-	public double getLeftJoystickY() {
-		return controllers[0].getLeftJoystickY();
-	}
-
+		/**getLeftJoyStickY - returns the y value of the left joystick of controller 0
+		 * 
+		 * @return
+		 */
+		public double getLeftJoystickY() {
+			return controllers[0].getLeftJoystickY();
+		}
+		/** getRightJoystickX - returns the x value of the right joystick of controller 0
+		 * @return
+		 */
+		public double getRightJoystickX() {
+			return controllers[0].getRightJoystickX();
+		}
+		/**getLeftJoystickY - returns the y value of the right joystick of controller 0
+		 * @return
+		 */
+		public double getRightJoystickY() {
+			return controllers[0].getRightJoystickY();
+		}
 }
