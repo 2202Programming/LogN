f3a8f3f2a3ceb9b207f07471cd84093ee3eee92a
leinad2018 <leinad2018@gmail.com>
2017-02-14 19:13:37 -0600

    Implemented Derek's PID values and speed mode

--- src/physicalOutput/TalonSRX.java
+++ src/physicalOutput/TalonSRX.java
@@ -19,23 +19,48 @@ public class TalonSRX extends IMotor {
 	public TalonSRX(int x, boolean reverse, boolean hasEncoder) {
 		this(x, reverse);
 		if (hasEncoder) {
-			part.configEncoderCodesPerRev(1024);
+			part.configEncoderCodesPerRev(4096);
 			part.setProfile(0);
-			part.setF(0.008562);
-			part.setP(0.02046);
-			part.setI(0);
-			part.setD(0);
+			part.setF(0.013);    /// 0.008562
+			part.setP(0.018);    // 102/4000  .1*1023/4000Uerror  = 0.0255
+			part.setI(0.00002);   // set P/100 to start
+			part.setD(0.0000);
+			part.setAllowableClosedLoopErr(0);
 			
-			part.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+			part.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 			part.reverseSensor(false);
 			part.changeControlMode(TalonControlMode.Speed);
+			
+			//limit the error wind up, greater than IZone, zeros the integrator.
+			//
+			//    err= 500RPM    (50r/min)*(4096U/rev)*(1/60 min/s)*(1/10 s/100ms)
+			//       =  3410  U/100ms    (U is pulses or native units) 
+			//			
+			part.setIZone(0);   //should be in native-units (pulses/100ms) 
+			//Izone seems to break the CL control by zeroing the iAcc when the err gets to zero
+			//this feels like a bug in the SRX.  - Derek
+			part.setEncPosition(0);
+			part.ClearIaccum();
 		}
+		
 
 		this.hasEncoder = hasEncoder;
+	}
+	
+	public void teleopInit(){
+		super.teleopInit();
+		part.setEncPosition(0);
+		part.ClearIaccum();
+		part.clearStickyFaults();
+		part.setAllowableClosedLoopErr(20);
 	}
 
 	@Override
 	protected void setMotor(double x) {
 		part.set(x);
+	}
+
+	public double getSpeed() {
+		return part.getSpeed();
 	}
 }
