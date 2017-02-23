8285b21e5045ca03c8eef0dc9c69a0d6f2aa0fc8
leinad2018 <leinad2018@gmail.com>
2017-02-23 16:48:41 -0600

    Drive at angle improved by using pid controller

--- src/auto/commands/DriveAtAngle.java
+++ src/auto/commands/DriveAtAngle.java
@@ -2,6 +2,7 @@ package auto.commands;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import PID.PIDController;
 import auto.ICommand;
 import auto.IStopCondition;
 import comms.SmartWriter;
@@ -17,23 +18,68 @@ public class DriveAtAngle implements ICommand {
 	private double slowSpeed, fastSpeed;
 	private double angle;
 	private AHRS navx;
+	private PIDController controller;
+	private boolean usePID;
 	
+	/**
+	 * Creates the command using pid controlled angle
+	 * @param stop
+	 * @param speed
+	 * @param angle
+	 */
+	public DriveAtAngle(IStopCondition stop, double speed, double angle){
+		usePID = true;
+		// these will most likely be small as the value needs to be under 1.0/ -1.0
+		controller = new PIDController(0,0,0,true,false);
+		stopCondition = stop;
+		this.angle = angle;
+		slowSpeed = speed;
+	}
+	
+	/**
+	 * Creates the command that waddles back and forth
+	 * @param stop
+	 * @param slowSpeed
+	 * @param fastSpeed
+	 * @param angle
+	 */
 	public DriveAtAngle(IStopCondition stop, double slowSpeed, double fastSpeed, double angle) {
+		usePID = false;
 		stopCondition=stop;
 		this.slowSpeed=slowSpeed;
 		this.fastSpeed=fastSpeed;
-		navx=(AHRS)(SensorController.getInstance().getSensor("NAVX"));
 		this.angle=angle;
 	}
 	
 	public void init() {
 		stopCondition.init();
+		navx = (AHRS)(SensorController.getInstance().getSensor("NAVX"));
 		drive=(IDrive)Global.controlObjects.get("DRIVE");
 		drive.setDriveControl(DriveControl.EXTERNAL_CONTROL);
 	}
 
 	public boolean run() {
 		SmartWriter.putS("TargetAngle driveAtAngle ", getError()+"");
+		if(usePID){
+			withGyro();
+		}else{
+			nonGyro();
+		}
+		
+		if (stopCondition.stopNow()) {
+			stop();
+			return true;
+		}
+		return false;
+	}
+	
+	private void withGyro(){
+		double change = controller.calculate(0, getError());
+		drive.setLeftMotors(slowSpeed-change);
+		drive.setRightMotors(slowSpeed+change);
+	}
+	
+	private void nonGyro(){
 		if (getError()>0) {
 			drive.setLeftMotors(slowSpeed);
 			drive.setRightMotors(fastSpeed);
@@ -42,19 +88,26 @@ public class DriveAtAngle implements ICommand {
 			drive.setLeftMotors(fastSpeed);
 			drive.setRightMotors(slowSpeed);
 		}
-		if (stopCondition.stopNow()) {
-			stop();
-			return true;
-		}
-		return false;
 	}
 	
+	/**
+	 * Returns the angle off the set point from -180 to 180
+	 * @return error
+	 */
 	public double getError() {
 		double angle=navx.getAngle();
 		if (angle>180) {
 			angle=angle-360;
 		}
 		return angle-this.angle;
+	}
+	
+	/**
+	 * sets the new angle for the robot to drive to<br>
+	 * must be -180 to 180 at this point
+	 */
+	public void setAngle(double angleIn){
+		angle = angleIn;
 	}
 	
 	public void stop(){
