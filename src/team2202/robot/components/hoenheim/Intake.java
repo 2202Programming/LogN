8c4f3bf399e3ff90eca66a762eaf929165153aea
BeastRomulus <beastromulus@gmail.com>
2017-04-05 18:55:37 -0500

    Intake stuff for Tommy

--- src/team2202/robot/components/hoenheim/Intake.java
+++ src/team2202/robot/components/hoenheim/Intake.java
@@ -1,33 +1,72 @@
 package team2202.robot.components.hoenheim;
 
+import comms.XboxController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import physicalOutput.SolenoidController;
 import physicalOutput.motors.IMotor;
 import robot.IControl;
 
 public class Intake extends IControl {
 	private IMotor motor;
 	private int isOn;
-
-	public Intake(IMotor motor) {
-		this.motor = motor;
-	}
-
+	private boolean shouldBeOpen, shooting;
+	private XboxController controller;
+	private SolenoidController solenoidController;
 	/**
-	 * Sets the intake to intake, off, or reverse<br>
-	 * Preconditions: isOn is one of the three stated values<br>
-	 * Postconditions: if not, the intake will not work
-	 * 
-	 * @param isOn
-	 *            1: intake, 0: off, -1: reverse
+	 * Get will return true when it is open
 	 */
-	public void setOn(int isOn) {
-		this.isOn = isOn;
+	private DigitalInput limitSwitch;
+	
+	public Intake(IMotor motor, DigitalInput limitSwitch) {
+		this.motor = motor;
+		controller=XboxController.getXboxController();
+		solenoidController=SolenoidController.getInstance();
+		this.limitSwitch=limitSwitch;
+	}
+	
+	/**
+	 * Call this and pass in true when you start shooting, and then when openingInProgress returns false, you can shoot.
+	 * Make sure to call it and pass in false when done shooting.
+	 * @param shooting Are we shooting?
+	 * @return true if we are at the place we are told to be
+	 */
+	public boolean setShooting(boolean shooting) {
+		this.shooting=shooting;
+		shouldBeOpen=shooting;
+		isOn=0;
+		return limitSwitch.get()==shouldBeOpen;
 	}
 
 	public void teleopInit() {
 		isOn = 0;
+		shooting=false;
+		shouldBeOpen=false;
 	}
 
 	public void teleopPeriodic() {
 		motor.set(isOn);
+		if (!shooting) {
+			if (controller.getAHeld()) {
+				if (shouldBeOpen) {
+					isOn=1;
+				}
+				else {
+					isOn=-1;
+				}
+			}
+			else {
+				isOn=0;
+			}
+		}
+		else {
+			isOn=0;
+		}
+		try {
+			solenoidController.getSolenoid("intakeSolenoid").set(shouldBeOpen);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
+	
 }
